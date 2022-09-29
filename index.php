c4ef0123d6bfaff73acb945e1103ce280fd39df6
Jeyell 26 <john_louise_bungay@dlsu.edu.ph>
2022-09-29 21:11:21 +0800

    Fixing link embed thumbnail #1

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta property="og:image" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
     <title>ARWonderland</title>
     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
